734cb430297b7d509a277c81753e65b95557f20c
Gabriel Airton <gairton10@gmail.com>
2019-11-06 15:33:24 -0300

    Funcao que calcula a distancia

---
+++ Distancia.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//função que calcula a distancia
+float calculaDistancia (int x1, int y1, int x2, int y2)
+{
+	return sqrt(pow (x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+//main
+int main ()
+{
+
+}
